8aa33169f97267f29cb7fc0ddac0d77eafa860e5
Vincent Robert <vincent.robert@nanego.com>
2018-10-17 15:03:21 +0200

    Remove duplicated localized labels and refactor main index page

--- app/controllers/issuecloser_controller.rb
+++ app/controllers/issuecloser_controller.rb
@@ -2,13 +2,14 @@ class IssuecloserController < AdminController
   unloadable
 
   def index
-    if params.has_key?(:id) && params[:issue_action]=='change' && (Issue.find(params[:id]).status_id.to_s==Setting.plugin_issuecloser['issues_status_from'])
+    if params.has_key?(:id) && params[:issue_action] == 'change' && (Issue.find(params[:id]).status_id.to_s == Setting.plugin_issuecloser['issues_status_from'])
       Issue.find(params[:id]).update(status_id: Setting.plugin_issuecloser['issues_status_to'])
-    end      
-    @issues_all_c=Issue.all
-    @issues_to_change=Issue.where('status_id=?', Setting.plugin_issuecloser['issues_status_from']).where("updated_on < ?", Setting.plugin_issuecloser['auto_close_after_days'].to_i.days.ago).order(:created_on)
-    @issues_to_change_c=@issues_to_change.count
-    @issues_to_change_paginated=@issues_to_change.page params[:page]
+    end
+    @issues_all_count = Issue.count
+    @issues_to_change = Issue.where('status_id=?', Setting.plugin_issuecloser['issues_status_from']).where("updated_on < ?", Setting.plugin_issuecloser['auto_close_after_days'].to_i.days.ago).order(:created_on)
+    @issues_to_change_count = @issues_to_change.count
+    @issues_to_change_paginated = @issues_to_change.includes(:project, :tracker, :priority, :status).page params[:page]
+    @status_to = IssueStatus.find(Setting.plugin_issuecloser['issues_status_to'])
   end
 
 end
--- init.rb
+++ init.rb
@@ -5,9 +5,9 @@ Redmine::Plugin.register :issuecloser do
   version '0.0.1'
   url 'https://github.com/isheninp/redmine_issue_closer'
   author_url 'https://github.com/isheninp'
-  settings :default => {
-    :auto_close  => false,
-    :auto_close_after_days => 30,
-  }, 
-  :partial => 'settings/issuecloser'
+  settings :partial => 'settings/issuecloser',
+           :default => {
+               :auto_close => false,
+               :auto_close_after_days => 30,
+           }
 end
